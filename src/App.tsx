94412aaa0bd1683486482d355af7e831fc7c80c0
Gabriel Vale <gabriel.vale@OMEGAMETALURGIA.local>
2025-11-12 17:16:59 -0300

    added links

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
-import './App.css'
+import {
+  Container,
+  Link,
+} from "./App.ts";
 
 function App() {
 
   return (
-    <>
-      <div>Teste</div>
-    </>
+    <Container>
+        <Link href="../public/00.OMG FDS - Aluminio em Po/ALUMÍNIO EM PÓ - FDS - 004.pdf" target="_blank">
+          Almoxarifado
+        </Link>
+
+        <Link href="">
+          Cozinha
+        </Link>
+
+        <Link href="">
+          Geral
+        </Link>
+
+        <Link href="">
+          Laboratorio
+        </Link>
+
+        <Link href="">
+          Manutenção
+        </Link>
+
+        <Link href="">
+          Produção
+        </Link>
+    </Container>
   )
 }
 
