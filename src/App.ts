48cc838c1da4b5ab3baa08d6ff756afc6f9b9527
Gabriel Vale <gabriel.vale@OMEGAMETALURGIA.local>
2025-11-13 17:31:15 -0300

    styles

--- src/App.ts
+++ src/App.ts
@@ -2,6 +2,9 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: black;
     
 `;
 
