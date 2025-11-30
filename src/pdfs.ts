0c450fe560ddde241728bdd728ada8df7549b1a1
gabrielAlvees <gabrieldev.alves@gmail.com>
2025-11-30 20:50:46 -0300

    pdf added

--- src/pdfs.ts
+++ src/pdfs.ts
@@ -165,7 +165,121 @@ export const almoxarifadoPDFs = [
   {
     nome: "41. FISPQ Desinfetante  Audax Butterfly",
     pdf: "/pdfs/Almoxarifado/41. FISPQ Desinfetante  Audax Butterfly.pdf"
+  },
+  {
+  nome: "42. FDS Detergente Pia Líquido",
+  pdf: "/pdfs/Almoxarifado/42. FDS Detergente Pia Líquido.pdf"
+  },
+  {
+    nome: "43. FDS Detergente Limpa Pedra",
+    pdf: "/pdfs/Almoxarifado/43. FDS Detergente Limpa Pedra.pdf"
+  },
+  {
+    nome: "44. FISPQ Sabão em pó Omo Lav. Perf.",
+    pdf: "/pdfs/Almoxarifado/44. FISPQ Sabão em pó Omo Lav. Perf..pdf"
+  },
+  {
+    nome: "45. FISPQ Álcool em gel Bio Savon",
+    pdf: "/pdfs/Almoxarifado/45. FISPQ Alcool em gel Bio Savon..pdf"
+  },
+  {
+    nome: "46. FISPQ Removedor Gitanes",
+    pdf: "/pdfs/Almoxarifado/46. FISPQ Removedor Gitanes.pdf"
+  },
+  {
+    nome: "47. FISPQ Pedra Sanitária",
+    pdf: "/pdfs/Almoxarifado/47. FISPQ Pedra Sanitária.pdf"
+  },
+  {
+    nome: "49. FISPQ Tinta Spray Alumínio",
+    pdf: "/pdfs/Almoxarifado/49. FISPQ Tinta Spray Alumínio.pdf"
+  },
+  {
+    nome: "50. FDS Tinta Spray Verde",
+    pdf: "/pdfs/Almoxarifado/50. FDS Tinta Spray Verde.pdf"
+  },
+  {
+    nome: "51. FDS Tinta Spray Lilás",
+    pdf: "/pdfs/Almoxarifado/51. FDS Tinta Spray Lilás.pdf"
+  },
+  {
+    nome: "52. FDS Tinta Spray Vermelho",
+    pdf: "/pdfs/Almoxarifado/52. FDS Tinta Spray Vermelho.pdf"
+  },
+  {
+    nome: "53. FISPQ Tinta Spray Branco Quick Color",
+    pdf: "/pdfs/Almoxarifado/53. FISPQ Tinta Spray Branco Quick Color.pdf"
+  },
+  {
+    nome: "54. FISPQ Tinta Spray Mult. Fundo Prep.",
+    pdf: "/pdfs/Almoxarifado/54. FISPQ Tinta Spray Mult. Fundo Prep..pdf"
+  },
+  {
+  nome: "55. FDS Tinta Spray Preto",
+  pdf: "/pdfs/Almoxarifado/55. FDS Tinta Spray Preto.pdf"
+  },
+  {
+    nome: "56. FDS Tinta Spray Azul Claro",
+    pdf: "/pdfs/Almoxarifado/56. FDS Tinta Spray Azul Claro.pdf"
+  },
+  {
+    nome: "57. FDS Tinta Spray Amarelo",
+    pdf: "/pdfs/Almoxarifado/57. FDS Tinta Spray Amarelo.pdf"
+  },
+  {
+    nome: "58. FDS Tinta Spray Azul Escuro",
+    pdf: "/pdfs/Almoxarifado/58. FDS Tinta Spray Azul Escuro.pdf"
+  },
+  {
+    nome: "59. FDS Tinta Spray Cinza Escuro",
+    pdf: "/pdfs/Almoxarifado/59. FDS Tinta Spray Cinza Escuro.pdf"
+  },
+  {
+    nome: "60. FDS Tinta Spray Laranja",
+    pdf: "/pdfs/Almoxarifado/60. FDS Tinta Spray Laranja.pdf"
+  },
+  {
+    nome: "61. FDS Tinta Spray Marrom",
+    pdf: "/pdfs/Almoxarifado/61. FDS Tinta Spray Marrom.pdf"
+  },
+  {
+    nome: "62. FISPQ Tinta Piso Amar. Demarca",
+    pdf: "/pdfs/Almoxarifado/62. FISPQ Tinta Piso Amar. Demarca..pdf"
+  },
+  {
+    nome: "63. FISPQ Primer Univer. Cinza FARBEN",
+    pdf: "/pdfs/Almoxarifado/63. FISPQ Primer Univer. Cinza FARBEN.pdf"
+  },
+  {
+    nome: "64. FISPQ Adesivo Compound - Tecbond",
+    pdf: "/pdfs/Almoxarifado/64. FISPQ Adesivo Compound - Tecbond.pdf"
+  },
+  {
+    nome: "65. FISPQ Tinta Alta Temp. Alumínio 750º",
+    pdf: "/pdfs/Almoxarifado/65. FISPQ Tinta Alta Temp. Aluminio 750.pdf"
+  },
+  {
+    nome: "66. FISPQ Zarcao Sintético Serralheiro",
+    pdf: "/pdfs/Almoxarifado/66. FISPQ Zarcao Sintetico Serralheiro.pdf"
+  },
+  {
+    nome: "67. FISPQ Tinta Esmalte Sint. Cinza Brilhante",
+    pdf: "/pdfs/Almoxarifado/67. FISPQ Tinta Esmalte Sint. Cinza Brilhante.pdf"
+  },
+  {
+    nome: "68. FDS Tinta Epoxi Fundo Misto Verm.",
+    pdf: "/pdfs/Almoxarifado/68. FDS Tinta Epoxi Fundo Misto Verm..pdf"
+  },
+  {
+    nome: "69. FISPQ Tinta Acrílica Azul Franca",
+    pdf: "/pdfs/Almoxarifado/69. FISPQ Tinta Acrilica Azul Franca.pdf"
+  },
+  {
+    nome: "70. ",
+    pdf: ""
+  },
+  {
+    nome: "71. FISPQ Tinta Esmalte Sintético",
+    pdf: "/pdfs/Almoxarifado/71. FISPQ Tinta Esmalte Sintetico .pdf"
   }
-  
-  
 ];
