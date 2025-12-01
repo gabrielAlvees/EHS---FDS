2f5f717f465d4bd597883fddd4c551cd3811bc47
gabrielAlvees <gabrieldev.alves@gmail.com>
2025-11-30 21:25:49 -0300

    enumered numbers of dinner

--- src/CozinhaPdfs.ts
+++ src/CozinhaPdfs.ts
@@ -1,34 +1,34 @@
 export const pdfsCozinha = [
   {
-    nome: "129. FISPQ - NEUTERGEN-DN GEL",
+    nome: "1. FISPQ - NEUTERGEN-DN GEL",
     pdf: "/pdfs/Cozinha/FISPQ - NEUTERGEN-DN GEL - 1.pdf"
   },
   {
-    nome: "130. FISPQ - NIPPO-CLOR",
+    nome: "2. FISPQ - NIPPO-CLOR",
     pdf: "/pdfs/Cozinha/FISPQ - NIPPO-CLOR - 2.pdf"
   },
   {
-    nome: "131. FISPQ - NIPPO-FAT GRILL",
+    nome: "3. FISPQ - NIPPO-FAT GRILL",
     pdf: "/pdfs/Cozinha/FISPQ - NIPPO-FAT GRILL - 3.pdf"
   },
   {
-    nome: "132. FISPQ - NIPPO-LAT 3000 K",
+    nome: "4. FISPQ - NIPPO-LAT 3000 K",
     pdf: "/pdfs/Cozinha/FISPQ - NIPPO-LAT 3000 K - 4.pdf"
   },
   {
-    nome: "133. FISPQ - NIPPO-LAT GF 40 CLEAR",
+    nome: "5. FISPQ - NIPPO-LAT GF 40 CLEAR",
     pdf: "/pdfs/Cozinha/FISPQ - NIPPO-LAT GF 40 CLEAR - 5.pdf"
   },
   {
-    nome: "134. FISPQ - NIPPO-LAT IB",
+    nome: "6. FISPQ - NIPPO-LAT IB",
     pdf: "/pdfs/Cozinha/FISPQ - NIPPO-LAT IB - 6.pdf"
   },
   {
-    nome: "135. FISPQ - NITROL AL",
+    nome: "7. FISPQ - NITROL AL",
     pdf: "/pdfs/Cozinha/FISPQ_-_NITROL_AL_- 7.pdf"
   },
   {
-    nome: "136. FISPQ GHS - Álcool 70°",
+    nome: "8. FISPQ GHS - Álcool 70°",
     pdf: "/pdfs/Cozinha/FISPQ_GHS-ÁLCOOL-70° - 8.pdf"
   }
 ];
