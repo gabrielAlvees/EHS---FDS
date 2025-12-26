b3dab123d725cd81ee7a941fb7139a641896d4d8
gabrielAlvees <gabrieldev.alves@gmail.com>
2025-12-25 21:21:08 -0300

    pdfs

--- src/ManutencaoPdfs.ts
+++ src/ManutencaoPdfs.ts
@@ -1,6 +1,314 @@
 export const pdfsManutencao = [
-  {
-    nome: "01. FDS Oxigenio Liquido",
-    pdf: "pdfs/Manutencao/01. FDS Oxigenio Liquido/01. FDS Oxigenio Liquido.pdf"
+{
+    nome: "01. FISPQ Oxigenio Liquido",
+    pdf: "/pdfs/Manutencao/01. FISPQ Oxigenio Liquido.pdf"
   },
+  {
+    nome: "02. FISPQ Argonio 256431",
+    pdf: "/pdfs/Manutencao/02. FISPQ Argonio 256431.pdf"
+  },
+  {
+    nome: "03. FISPQ Nitrogênio 256452",
+    pdf: "/pdfs/Manutencao/03. FISPQ Nitrogênio 256452.pdf"
+  },
+  {
+    nome: "04. FISPQ Oxigênio",
+    pdf: "/pdfs/Manutencao/04. FISPQ Oxigênio.pdf"
+  },
+  {
+    nome: "05. FISPQ Argonio 256535",
+    pdf: "/pdfs/Manutencao/05. FISPQ Argonio 256535.pdf"
+  },
+  {
+    nome: "06. FISPQ Acetileno 405951",
+    pdf: "/pdfs/Manutencao/06. FISPQ Acetileno 405951.pdf"
+  },
+  {
+    nome: "07. FISPQ Mistura de Gases 417923",
+    pdf: "/pdfs/Manutencao/07. FISPQ Mistura de Gases 417923.pdf"
+  },
+  {
+    nome: "08. FISPQ Catalisador  Mek",
+    pdf: "/pdfs/Manutencao/08. FISPQ Catalisador  Mek.pdf"
+  },
+  {
+    nome: "09. FISPQ Massa Plastica branca",
+    pdf: "/pdfs/Manutencao/09. FISPQ Massa Plastica branca.pdf"
+  },
+  {
+    nome: "10. FISPQ Gel Desengr. Premium Citrus",
+    pdf: "/pdfs/Manutencao/10. FISPQ Gel Desengr. Premium Citrus.pdf"
+  },
+  {
+    nome: "11. FISPQ Gel Desengr.  Premium",
+    pdf: "/pdfs/Manutencao/11. FISPQ Gel Desengr.  Premium.pdf"
+  },
+  {
+    nome: "12. FISPQ Sabão em Pasta Desengrax",
+    pdf: "/pdfs/Manutencao/12. FISPQ Sabão em Pasta Desengrax.pdf"
+  },
+  {
+    nome: "14. FISPQ Adesivo Silicone Ac. Incolor",
+    pdf: "/pdfs/Manutencao/14. FISPQ Adesivo Silicone Ac. Incolor.pdf"
+  },
+  {
+    nome: "15. FISPQ Lubrific. Tilub HL-MO",
+    pdf: "/pdfs/Manutencao/15. FISPQ Lubrific. Tilub HL-MO.pdf"
+  },
+  {
+    nome: "16. FISPQ Lubrific. TILUB MOL G Plus",
+    pdf: "/pdfs/Manutencao/16. FISPQ Lubrific. TILUB MOL G Plus.pdf"
+  },
+  {
+    nome: "17. FISPQ Fermel ALF 10",
+    pdf: "/pdfs/Manutencao/17. FISPQ Fermel ALF 10.pdf"
+  },
+  {
+    nome: "18. FISPQ Sabão Deseng. Pinheiro",
+    pdf: "/pdfs/Manutencao/18. FISPQ Sabão Deseng. Pinheiro.pdf"
+  },
+  {
+    nome: "19. FISPQ Silicone Pinheiro",
+    pdf: "/pdfs/Manutencao/19. FISPQ Silicone Pinheiro.pdf"
+  },
+  {
+    nome: "20. FISPQ Pretinho Pinheiro",
+    pdf: "/pdfs/Manutencao/20. FISPQ Pretinho Pinheiro.pdf"
+  },
+  {
+    nome: "21. FISPQ Massa Plast Refrat 70% AL203",
+    pdf: "/pdfs/Manutencao/21. FISPQ Massa Plast Refrat 70% AL203.pdf"
+  },
+  {
+    nome: "49. FISPQ Tinta Spray Aluminio",
+    pdf: "/pdfs/Manutencao/49. FISPQ Tinta Spray Aluminio.pdf"
+  },
+  {
+    nome: "50. FDS Tinta Spray Verde",
+    pdf: "/pdfs/Manutencao/50. FDS Tinta Spray Verde.pdf"
+  },
+  {
+    nome: "51. FDS Tinta Spray Lilas",
+    pdf: "/pdfs/Manutencao/51. FDS Tinta Spray Lilas.pdf"
+  },
+  {
+    nome: "52. FDS Tinta Spray Vermelho",
+    pdf: "/pdfs/Manutencao/52. FDS Tinta Spray Vermelho.pdf"
+  },
+  {
+    nome: "53. FISPQ Tinta Spray Branco Quick Color",
+    pdf: "/pdfs/Manutencao/53. FISPQ Tinta Spray Branco Quick Color.pdf"
+  },
+  {
+    nome: "55. FDS Tinta Spray Preto",
+    pdf: "/pdfs/Manutencao/55. FDS Tinta Spray Preto.pdf"
+  },
+  {
+    nome: "56. FDS Tinta Spray Azul Claro",
+    pdf: "/pdfs/Manutencao/56. FDS Tinta Spray Azul Claro.pdf"
+  },
+  {
+    nome: "57. FDS Tinta Spray Amarelo",
+    pdf: "/pdfs/Manutencao/57. FDS Tinta Spray Amarelo.pdf"
+  },
+  {
+    nome: "58. FDS Tinta Spray Azul Escuro",
+    pdf: "/pdfs/Manutencao/58. FDS Tinta Spray Azul Escuro.pdf"
+  },
+  {
+    nome: "59. FDS Tinta Spray Cinza Escuro",
+    pdf: "/pdfs/Manutencao/59. FDS Tinta Spray Cinza Escuro.pdf"
+  },
+  {
+    nome: "60.FDS Tinta Spray Laranja",
+    pdf: "/pdfs/Manutencao/60.FDS Tinta Spray Laranja.pdf"
+  },
+  {
+    nome: "61. FDS Tinta Spray Marrom",
+    pdf: "/pdfs/Manutencao/61. FDS Tinta Spray Marrom.pdf"
+  },
+  {
+    nome: "62. FISPQ Tinta Piso Amar. Demarca. - Indutil",
+    pdf: "/pdfs/Manutencao/62. FISPQ Tinta Piso Amar. Demarca. - Indutil.pdf"
+  },
+  {
+    nome: "63. FISPQ Primer Univer. Cinza FARBEN",
+    pdf: "/pdfs/Manutencao/63. FISPQ Primer Univer. Cinza FARBEN.pdf"
+  },
+  {
+    nome: "64. FISPQ Adesivo Compound - Tecbond MF",
+    pdf: "/pdfs/Manutencao/64. FISPQ Adesivo Compound - Tecbond MF.pdf"
+  },
+  {
+    nome: "65. FISPQ Tinta Alta Temp. Aluminio  750°C",
+    pdf: "/pdfs/Manutencao/65. FISPQ Tinta Alta Temp. Aluminio  750°C.pdf"
+  },
+  {
+    nome: "66. FISPQ Zarcao Sintetico Serralheiro",
+    pdf: "/pdfs/Manutencao/66. FISPQ Zarcao Sintetico Serralheiro.pdf"
+  },
+  {
+    nome: "67. FISPQ Tinta Esmalte Sint. Cinza Brilh..",
+    pdf: "/pdfs/Manutencao/67. FISPQ Tinta Esmalte Sint. Cinza Brilh...pdf"
+  },
+  {
+    nome: "68.FDS Tinta Epoxi Fundo Misto Verm..",
+    pdf: "/pdfs/Manutencao/68.FDS Tinta Epoxi Fundo Misto Verm...pdf"
+  },
+  {
+    nome: "69. FISPQ Tinta Acrilica Azul Franca",
+    pdf: "/pdfs/Manutencao/69. FISPQ Tinta Acrilica Azul Franca.pdf"
+  },
+  {
+    nome: "71. FISPQ Tinta Esmalte Sintetico ",
+    pdf: "/pdfs/Manutencao/71. FISPQ Tinta Esmalte Sintetico .pdf"
+  },
+  {
+    nome: "72. FISPQ Esmalte Sint. B.",
+    pdf: "/pdfs/Manutencao/72. FISPQ Esmalte Sint. B..pdf"
+  },
+  {
+    nome: "73. FISPQ Tinta Esmalte Sintetico",
+    pdf: "/pdfs/Manutencao/73. FISPQ Tinta Esmalte Sintetico.pdf"
+  },
+  {
+    nome: "74. FISPQ Tinta Acrilica Branca",
+    pdf: "/pdfs/Manutencao/74. FISPQ Tinta Acrilica Branca.pdf"
+  },
+  {
+    nome: "77. FDSTinta Epoxi Azul Temp. 120C",
+    pdf: "/pdfs/Manutencao/77. FDSTinta Epoxi Azul Temp. 120C.pdf"
+  },
+  {
+    nome: "77. FISPQ Tinta Alta Temp. Aluminio 400°C",
+    pdf: "/pdfs/Manutencao/77. FISPQ Tinta Alta Temp. Aluminio 400°C.pdf"
+  },
+  {
+    nome: "79. FDS Oleo Sintetico ROTOX TEND DUTY",
+    pdf: "/pdfs/Manutencao/79. FDS Oleo Sintetico ROTOX TEND DUTY.pdf"
+  },
+  {
+    nome: "81. FDS Graxa MP 2 AZUL- BLUTHER",
+    pdf: "/pdfs/Manutencao/81. FDS Graxa MP 2 AZUL- BLUTHER.pdf"
+  },
+  {
+    nome: "83. FISPQ Thinner Premium Eucatex",
+    pdf: "/pdfs/Manutencao/83. FISPQ Thinner Premium Eucatex.pdf"
+  },
+  {
+    nome: "84. FISPQ Aguarraz Itaquá",
+    pdf: "/pdfs/Manutencao/84. FISPQ Aguarraz Itaquá.pdf"
+  },
+  {
+    nome: "85. FISPQ Fluido de Corte  2",
+    pdf: "/pdfs/Manutencao/85. FISPQ Fluido de Corte  2.pdf"
+  },
+  {
+    nome: "86. FISPQ Querosene Polaris",
+    pdf: "/pdfs/Manutencao/86. FISPQ Querosene Polaris.pdf"
+  },
+  {
+    nome: "87. FISPQ Thinner Limpeza Itaqua",
+    pdf: "/pdfs/Manutencao/87. FISPQ Thinner Limpeza Itaqua.pdf"
+  },
+  {
+    nome: "88.FDSTinta Esmalte Si",
+    pdf: "/pdfs/Manutencao/88.FDSTinta Esmalte Si.pdf"
+  },
+  {
+    nome: "89. FISPQ Tinta Epoxi Cinza Temp. 120°C",
+    pdf: "/pdfs/Manutencao/89. FISPQ Tinta Epoxi Cinza Temp. 120°C.pdf"
+  },
+  {
+    nome: "90. FISPQ Tinta Esmalte Sintetico Aluminio",
+    pdf: "/pdfs/Manutencao/90. FISPQ Tinta Esmalte Sintetico Aluminio.pdf"
+  },
+  {
+    nome: "91. FISPQ LUBRAX COMPSOR PAO 46",
+    pdf: "/pdfs/Manutencao/91. FISPQ LUBRAX COMPSOR PAO 46.pdf"
+  },
+  {
+    nome: "92. FISPQ LUBRAX GL 5 140",
+    pdf: "/pdfs/Manutencao/92. FISPQ LUBRAX GL 5 140.pdf"
+  },
+  {
+    nome: "93. FISPQ Oleo Transmisssão UNITRACTOR 10",
+    pdf: "/pdfs/Manutencao/93. FISPQ Oleo Transmisssão UNITRACTOR 10.pdf"
+  },
+  {
+    nome: "94. FISPQ LUBRAX GL 5 80W90",
+    pdf: "/pdfs/Manutencao/94. FISPQ LUBRAX GL 5 80W90.pdf"
+  },
+  {
+    nome: "96. FISPQ LUBRAX GEAR 460",
+    pdf: "/pdfs/Manutencao/96. FISPQ LUBRAX GEAR 460.pdf"
+  },
+  {
+    nome: "97. FISPQ LUBRAX EXTRA TURBO CH-4",
+    pdf: "/pdfs/Manutencao/97. FISPQ LUBRAX EXTRA TURBO CH-4.pdf"
+  },
+  {
+    nome: "99. FISPQ Lubrax Turbo 20W-50",
+    pdf: "/pdfs/Manutencao/99. FISPQ Lubrax Turbo 20W-50.pdf"
+  },
+  {
+    nome: "100. FISPQ Oleo SAE 15W40 Semi-Sintetico",
+    pdf: "/pdfs/Manutencao/100. FISPQ Oleo SAE 15W40 Semi-Sintetico.pdf"
+  },
+  {
+    nome: "101. FDS Fluido de Freio DOT-3",
+    pdf: "/pdfs/Manutencao/101. FDS Fluido de Freio DOT-3.pdf"
+  },
+  {
+    nome: "102. FDS Fluido de Freio DOT-4",
+    pdf: "/pdfs/Manutencao/102. FDS Fluido de Freio DOT-4.pdf"
+  },
+  {
+    nome: "103. FISPQ Aditivo Super Rad",
+    pdf: "/pdfs/Manutencao/103. FISPQ Aditivo Super Rad.pdf"
+  },
+  {
+    nome: "104. FISPQ Aditivo de Bicos Injetores HT 2030",
+    pdf: "/pdfs/Manutencao/104. FISPQ Aditivo de Bicos Injetores HT 2030.pdf"
+  },
+  {
+    nome: "105. FISPQ Protetor Solar FPS 60",
+    pdf: "/pdfs/Manutencao/105. FISPQ Protetor Solar FPS 60.pdf"
+  },
+  {
+    nome: "106. FISPQ Creme Proteção Mãos - LUVEX",
+    pdf: "/pdfs/Manutencao/106. FISPQ Creme Proteção Mãos - LUVEX.pdf"
+  },
+  {
+    nome: "107. FISPQ Adesivo de Silicone Ac",
+    pdf: "/pdfs/Manutencao/107. FISPQ Adesivo de Silicone Ac.pdf"
+  },
+  {
+    nome: "108. FISPQ Piso acrilico premium- Amarelo Demarc..",
+    pdf: "/pdfs/Manutencao/108. FISPQ Piso acrilico premium- Amarelo Demarc..pdf"
+  },
+  {
+    nome: "109. FISPQ Pretinho Gitanes",
+    pdf: "/pdfs/Manutencao/109. FISPQ Pretinho Gitanes.pdf"
+  },
+  {
+    nome: "110. FISPQ Removedor Zulu sem cheiro",
+    pdf: "/pdfs/Manutencao/110. FISPQ Removedor Zulu sem cheiro.pdf"
+  },
+  {
+    nome: "111. FISPQ Thinner 700 Polaris",
+    pdf: "/pdfs/Manutencao/111. FISPQ Thinner 700 Polaris.pdf"
+  },
+  {
+    nome: "116. FISPQ Super Bonder Mega",
+    pdf: "/pdfs/Manutencao/116. FISPQ Super Bonder Mega.PDF"
+  },
+  {
+    nome: "117. TEKBOND - 793",
+    pdf: "/pdfs/Manutencao/117. TEKBOND - 793.pdf"
+  },
+  {
+    nome: "119. FISPQ Descarbonizante Spray - MP80",
+    pdf: "/pdfs/Manutencao/119. FISPQ Descarbonizante Spray - MP80.pdf"
+  }
 ]
